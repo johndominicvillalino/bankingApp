9a1d0c0bca64e21c6f8453e446f0de6c50ba5b75
John Villalino <johndominicvillalino@outlook.com>
2022-05-02 09:30:18 +0800

    refactor route

--- client/dbConnect/routes.js
+++ client/dbConnect/routes.js
@@ -1,6 +1,6 @@
 import {config} from './types.js'
 
-export const login = async (email,password) => {
+export const login = async ({email,password}) => {
    let body = {
       email,
       password
@@ -8,4 +8,11 @@ export const login = async (email,password) => {
    body = JSON.stringify(body)
    const res = await axios.post('api/login',body,config)
    return res.data;
+}
+
+export const stateUpdate = (pushTo,stateUpdate = { state: pushTo}) => {
+   history.pushState(pushTo, pushTo,pushTo);
+   let popStateEvent = new PopStateEvent("popstate", stateUpdate);
+   dispatchEvent(popStateEvent);
+
 }
--- client/scripts/scripts.js
+++ client/scripts/scripts.js
@@ -1,18 +1,18 @@
-import { login } from "../dbConnect/routes.js";
+import { login,stateUpdate } from "../dbConnect/routes.js";
 import { storeData, getStringData } from "../dbConnect/storage.js";
 const reloadState = window.location.href;
 const origin = window.location.origin;
 const dom = document;
 
+//link on click to trigger popstate
 dom.querySelectorAll("a").forEach((el) => {
   el.addEventListener("click", (e) => {
     const { id } = e.target;
-    history.pushState(`/${id}`, id, "/" + id);
-   let popStateEvent = new PopStateEvent("popstate", { state: `/${id}` });
-    dispatchEvent(popStateEvent);
+    stateUpdate(`/${id}`)
   });
 });
 
+//state update
 window.onpopstate = function (event) {
   let { state } = event;
   state = window.location.origin + state;
@@ -22,12 +22,22 @@ window.onpopstate = function (event) {
 
 window.addEventListener("load", (e) => {
   let state = getStringData("currentState");
+  let user = getStringData("user");
+
+  if(user) {
+    stateUpdate(`/dashboard`)
+    storeData("currentState", state);
+  } else {
+    stateUpdate('/')
+    storeData("currentState", state);
+  }
 
   if (reloadState !== state) {
     storeData("currentState", reloadState);
   }
-  
-  let popStateEvent = new PopStateEvent("popstate",{state : window.location.pathname});
+
+  const newStateState = {state: window.location.pathname }
+  let popStateEvent = new PopStateEvent("popstate",newStateState );
   dispatchEvent(popStateEvent);
 
 });
@@ -36,5 +46,48 @@ function processStateRoute() {
 
   if (getStringData("currentState") === origin + "/forgotPassword") {
     dom.getElementById("loginForm").style.display = "none";
+  } else {
+    dom.getElementById("loginForm").style.display = 'flex';
   }
 }
+
+dom.getElementById('loginSubmit').addEventListener('click', async e => {
+  e.preventDefault()
+  const data = {}
+  const inputs = dom.querySelectorAll('.loginform')
+  inputs.forEach(e => {
+    if (e.value.length < 1) {
+      e.style.border = '1px solid red'
+      return
+    }
+    data[e.name] = e.value
+    e.value = ''
+  })
+
+try {
+  const keys = Object.keys(data)
+  if(keys.length < 1) {
+    return
+  }
+  const res = await login(data)
+  if(res) {
+    storeData('user',res.id)
+    history.pushState(`/dashboard`, 'dashboard', "/dashboard");
+    let popStateEvent = new PopStateEvent("popstate", { state: `/dashboard` });
+    dispatchEvent(popStateEvent);
+    storeData("currentState", state);
+  }
+
+
+} catch (err) {
+  console.error(err.message)
+}
+  
+})
+
+//default color
+dom.querySelectorAll('.loginform').forEach(e => {
+  e.addEventListener('focus', el => {
+    e.style.border = '1px solid black'
+  })
+})
